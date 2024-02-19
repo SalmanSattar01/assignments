24290e6bb33f8829755c1efb84d5ed623b2b4485
salman sattar <salmansattar01@gmail.com>
2024-02-19 23:52:47 +0500

    added exercise no 10

---
+++ EXERCISE NO 10/q10.js
@@ -0,0 +1,13 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+// Define the area in sqft of a plot
+var areaSyard = 120;
+// Calculate the area in sqft of the sq yards using the formula sq yard * 9
+var areaSft = areaSyard * 9;
+// Print the calculated area in sqft
+console.log("The area of the Plot is:", areaSft);
+///////////////////////////////////////////////////////////////
+// Define when and who was solving exercise no 10
+var student = "Salman Sattar";
+var currentDate = new Date();
+// Print the detail
+console.log("The Exercise No 10 is written by ".concat(student, " on dated ").concat(currentDate));
