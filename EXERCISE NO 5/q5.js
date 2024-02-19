5ff82b806e29aa2f516847bd6e79ac8676aa50ae
salman sattar <salmansattar01@gmail.com>
2024-02-19 22:41:00 +0500

    added exercise no 5

---
+++ EXERCISE NO 5/q5.js
@@ -0,0 +1,5 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = "Albert Einstein";
+var quote2 = "Try not to become a man of success, but rather try to become a man of value.";
+var message = "".concat(famous_person, " once said, \"").concat(quote2, "\"");
+console.log(message);
