502ba33bd239c8d39e0fd1e415061d9d4c3ddf69
salman sattar <salmansattar01@gmail.com>
2024-02-19 22:22:32 +0500

    added exercise no 4

---
+++ EXERCISE NO 4/Q4.ts
@@ -0,0 +1,5 @@
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+let quote: string = "I do not believe in taking the right decision, I take a decision and make it right.";
+let author: string = "Muhammad Ali Jinnah";
+
+console.log(`"${quote}" - ${author}`);
