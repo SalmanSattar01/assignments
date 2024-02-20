a1fab57e8ceb257a403cfc0aad0cea6f06375564
SalmanSattar01 <160482645+SalmanSattar01@users.noreply.github.com>
2024-02-20 14:02:39 +0500

    added exercise no 20

---
+++ EXERCISE NO 20/Q20.ts
@@ -0,0 +1,17 @@
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+// Define an array containing the names of continents
+let continents: string[] = [
+    "Africa",
+    "Antarctica",
+    "Asia",
+    "Europe",
+    "North America",
+    "Australia (Oceania)",
+    "South America"
+];
+
+// Print the list of continents
+console.log("List of continents:");
+continents.forEach(continents => {
+    console.log(continents);
