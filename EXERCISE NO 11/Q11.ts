32189c54a29ade071eb42e570907026238b6dacf
SalmanSattar01 <160482645+SalmanSattar01@users.noreply.github.com>
2024-02-20 12:07:04 +0500

    Update Q11.ts

--- EXERCISE NO 11/Q11.ts
+++ EXERCISE NO 11/Q11.ts
@@ -1,1 +1,6 @@
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
 
+let names: string[] = ["Shoaib", "Shahzad", "Zeeshan", "Zahid", "Ali"];
+names.forEach(name => {
+    console.log(name);
+});
