6cdb24a273b1b04d4a81c2bb985dbcebfe26eafb
salman sattar <salmansattar01@gmail.com>
2024-02-22 00:51:20 +0500

    added java script file

---
+++ EXERCISE NO 21/Q21.js
@@ -0,0 +1,19 @@
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+// Create objects containing different items
+var myCellPhone = {
+    brand: "Samsung",
+    model: "A34",
+};
+var myCar = {
+    make: "Suzuki",
+    model: "Mehran",
+    year: 2010
+};
+var myFriends = {
+    childhood: "Shoaib",
+    new: "Zeeshan",
+};
+// Print out the objects
+console.log("My Cell Phone:", myCellPhone);
+console.log("My Car:", myCar);
+console.log("My Friends:", myFriends);
