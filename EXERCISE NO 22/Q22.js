3dc3efcb81d28aa3e25c6a2070effc776eb12329
salman sattar <salmansattar01@gmail.com>
2024-02-22 00:51:29 +0500

    added java script file

---
+++ EXERCISE NO 22/Q22.js
@@ -0,0 +1,6 @@
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+var colors = ["red", "green", "blue"];
+// Intentional error: accessing an index that doesn't exist
+console.log(colors[3]); // This will throw an error
+// Correcting the error by accessing a valid index
+console.log(colors[2]); // This will print "blue"
