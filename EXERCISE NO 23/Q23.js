cc23bdf647f6fa79019841416c054bf989b20d7b
salman sattar <salmansattar01@gmail.com>
2024-02-22 00:51:40 +0500

    added java script file

---
+++ EXERCISE NO 23/Q23.js
@@ -0,0 +1,42 @@
+//Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+//let car = 'subaru';
+//console.log("Is car == 'subaru'? I predict True.")
+//console.log(car == 'subaru')
+//• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+var Suzuki = 'Mehran';
+var Toyota = 'Corolla';
+// Test 1
+console.log("Is Suzuki equal to Corolla? I predict False.");
+console.log(Suzuki == 'Corolla');
+// Test 2
+console.log("Is Toyota equal to Mehran? I predict False.");
+console.log(Toyota == 'Mehran');
+var x = 5;
+var y = 10;
+// Test 3
+console.log("Is x equal to y? I predict False.");
+console.log(x == y);
+// Test 4
+console.log("Is y less than x? I predict False.");
+console.log(y < x);
+// Test 5
+console.log("Is x greater than or equal to y? I predict False.");
+console.log(x >= y);
+// Test 6
+console.log("Is y greater than or equal to x? I predict True.");
+console.log(y >= x);
+var fruit = 'apple';
+var vegetable = 'carrot';
+// Test 7
+console.log("Is fruit equal to 'apple'? I predict True.");
+console.log(fruit == 'apple');
+// Test 8
+console.log("Is vegetable equal to 'carrot'? I predict True.");
+console.log(vegetable == 'carrot');
+// Test 9
+console.log("Is fruit not equal to 'banana'? I predict True.");
+console.log(fruit != 'banana');
+// Test 10
+console.log("Is vegetable not equal to 'broccoli'? I predict True.");
+console.log(vegetable != 'broccoli');
